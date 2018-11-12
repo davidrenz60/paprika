0b87494f23ef17d3855a0fba8d649961d6fda7d0
David Renz <davidrenz60@gmail.com>
2018-11-12 13:52:02 -0800

    fix tests

--- spec/fabricators/user_fabricator.rb
+++ spec/fabricators/user_fabricator.rb
@@ -1,6 +1,7 @@
 Fabricator(:user) do
   username { Faker:: Internet.user_name }
   password { Faker::Internet.password }
+  email { Faker::Internet.email }
 end
 
 Fabricator(:admin, from: :user) do
